ab11b4b9732a701eb7c2f09c8fb1d1ee6c034d91
Kim McCaskill <kimmccaskill92@gmail.com>
2020-02-04 15:11:51 -0700

    Add AreasContainer component

---
+++ src/AreasContainer/AreasContainer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './AreasContainer.css'
+import AreaCard from '../AreaCard/AreaCard.js'
+
+const AreasContainer = ({ areas }) => {
+  const appendedAreas = areas.map(area => <AreaCard areaName={area.name}/>)
+  return(
+    <>
+      <h2 className="areas-title">Pick a neighborhood</h2>
+      <section className="areas-container">
+        {appendedAreas}
+      </section>
+    </>
+  )
+}
+
+export default AreasContainer;
