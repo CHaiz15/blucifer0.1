7be14a56ed9093df06f6e6f26cec6a9450cac507
Caleb Haizlett <caleb.haizlett@gmail.com>
2020-02-10 13:33:22 -0700

    Add images to ListingDetails

--- src/ListingDetails/ListingDetails.js
+++ src/ListingDetails/ListingDetails.js
@@ -7,6 +7,11 @@ const ListingDetails = ({listingId, listings}) => {
         <div>
             <div>
                 <h2>{selectedListing.name}</h2>
+                <div>
+                    <img src={process.env.PUBLIC_URL + `/images/${listingId}_a.jpg`} />
+                    <img src={process.env.PUBLIC_URL + `/images/${listingId}_b.jpg`} />
+                    <img src={process.env.PUBLIC_URL + `/images/${listingId}_c.jpg`} />
+                </div>
                 <p>Address: {selectedListing.address.street}, {selectedListing.address.zip}</p>
                 <p>{selectedListing.details.beds} bed {selectedListing.details.baths} bath</p>
                 <p>{selectedListing.details.cost_per_night} / Night</p>
