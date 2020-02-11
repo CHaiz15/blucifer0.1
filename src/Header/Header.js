999cb4cdb3e3167e73639f89f8409d0b4aa01a20
Caleb Haizlett <caleb.haizlett@gmail.com>
2020-02-11 13:07:20 -0700

    Add Favorite link

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import './Header.css';
 import logo from '../assets/DarkLogo.png'
+import { Link } from 'react-router-dom';
 
 const logout = () => {
     window.location.href = '/';
 }
 
-const Header = ({ name,purpose }) => {
+const Header = ({ name, purpose, totalFavorites }) => {
   return (
     <header className="header">
       <div className='welcome-holder'>
         <img className='blucifer-logo' src={logo} alt='logo'/>
         <h3 className='user-welcome'>Welcome, {name}!</h3>
       </div>
-      <h3>{purpose}, made easy.</h3>
+      <Link to='/nav/areas' className="logout-btn">{purpose}, made easy.</Link>
+      <Link to='/nav/favorites' className="logout-btn">Favorites({totalFavorites})</Link>
       <button className="logout-btn" onClick={logout}>Logout</button>
     </header>
   )
