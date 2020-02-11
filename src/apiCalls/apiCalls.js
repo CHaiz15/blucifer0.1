7049f0a521495b7c303b1716540c7e634a060ba9
Caleb Haizlett <caleb.haizlett@gmail.com>
2020-02-11 16:12:17 -0700

    Add getData

---
+++ src/apiCalls/apiCalls.js
@@ -0,0 +1,44 @@
+export const getData = () => {
+  return fetch('http://localhost:3001/api/v1/areas')
+    .then(response => {
+        if(!response.ok) {
+             throw Error('Error while fetching, did not get a 200 status code!')
+        }
+       return response.json()
+    })
+}
+
+export const fetchAreaDetails = (areaData) => {
+  const promises = areaData.areas.map(area => {
+    return fetch('http://localhost:3001' + area.details)
+      .then(response => response.json())
+      .then(areaDetails => {
+        return {
+          id: areaDetails.id,
+          shortName: area.area || area.name,
+          name: areaDetails.name,
+          location: areaDetails.location,
+          about: areaDetails.about,
+          listings: areaDetails.listings
+          }
+      })
+  })
+  return Promise.all(promises)
+}
+
+export const fetchListings = (listings) => {
+  const promises = listings.map(listing => {
+    return fetch('http://localhost:3001' + listing)
+      .then(response => response.json())
+      .then(listingDetails => {
+        return {
+          listing_id: listingDetails.listing_id,
+          area_id: listingDetails.area_id,
+          name: listingDetails.name,
+          address: listingDetails.address,
+          details: listingDetails.details,
+        }
+      })
+  })
+  return Promise.all(promises)
+}
